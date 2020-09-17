65ab92eec8e3895af2e8f0f93609f7d5dd6cabbd
Александр Минайчев <alexander.minaychev@gmail.com>
2020-09-17 15:14:40 +0700

    Upload of full app

---
+++ WordGuessApp/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace WordGuessApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxGuessWord = new System.Windows.Forms.TextBox();
+            this.labelGuess = new System.Windows.Forms.Label();
+            this.buttonSendAnswer = new System.Windows.Forms.Button();
+            this.richTextBoxHistory = new System.Windows.Forms.RichTextBox();
+            this.buttonSetPuzzleWord = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxGuessWord
+            // 
+            this.textBoxGuessWord.Location = new System.Drawing.Point(132, 79);
+            this.textBoxGuessWord.Name = "textBoxGuessWord";
+            this.textBoxGuessWord.Size = new System.Drawing.Size(198, 20);
+            this.textBoxGuessWord.TabIndex = 0;
+            this.textBoxGuessWord.TextChanged += new System.EventHandler(this.textBoxGuessWord_TextChanged);
+            this.textBoxGuessWord.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxGuessWord_KeyDown);
+            // 
+            // labelGuess
+            // 
+            this.labelGuess.AutoSize = true;
+            this.labelGuess.Location = new System.Drawing.Point(43, 79);
+            this.labelGuess.Name = "labelGuess";
+            this.labelGuess.Size = new System.Drawing.Size(71, 13);
+            this.labelGuess.TabIndex = 1;
+            this.labelGuess.Text = "Я думаю это";
+            // 
+            // buttonSendAnswer
+            // 
+            this.buttonSendAnswer.Location = new System.Drawing.Point(373, 77);
+            this.buttonSendAnswer.Name = "buttonSendAnswer";
+            this.buttonSendAnswer.Size = new System.Drawing.Size(75, 22);
+            this.buttonSendAnswer.TabIndex = 2;
+            this.buttonSendAnswer.Text = "Отправить";
+            this.buttonSendAnswer.UseVisualStyleBackColor = true;
+            this.buttonSendAnswer.Click += new System.EventHandler(this.buttonSendAnswer_Click);
+            // 
+            // richTextBoxHistory
+            // 
+            this.richTextBoxHistory.Location = new System.Drawing.Point(132, 147);
+            this.richTextBoxHistory.Name = "richTextBoxHistory";
+            this.richTextBoxHistory.Size = new System.Drawing.Size(198, 213);
+            this.richTextBoxHistory.TabIndex = 3;
+            this.richTextBoxHistory.Text = "";
+            this.richTextBoxHistory.VScroll += new System.EventHandler(this.textBoxGuessWord_TextChanged);
+            // 
+            // buttonSetPuzzleWord
+            // 
+            this.buttonSetPuzzleWord.Location = new System.Drawing.Point(132, 12);
+            this.buttonSetPuzzleWord.Name = "buttonSetPuzzleWord";
+            this.buttonSetPuzzleWord.Size = new System.Drawing.Size(198, 28);
+            this.buttonSetPuzzleWord.TabIndex = 4;
+            this.buttonSetPuzzleWord.Text = "Загадать слово";
+            this.buttonSetPuzzleWord.UseVisualStyleBackColor = true;
+            this.buttonSetPuzzleWord.Click += new System.EventHandler(this.buttonSetPuzzleWord_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(490, 391);
+            this.Controls.Add(this.buttonSetPuzzleWord);
+            this.Controls.Add(this.richTextBoxHistory);
+            this.Controls.Add(this.buttonSendAnswer);
+            this.Controls.Add(this.labelGuess);
+            this.Controls.Add(this.textBoxGuessWord);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxGuessWord;
+        private System.Windows.Forms.Label labelGuess;
+        private System.Windows.Forms.Button buttonSendAnswer;
+        private System.Windows.Forms.RichTextBox richTextBoxHistory;
+        private System.Windows.Forms.Button buttonSetPuzzleWord;
+    }
+}
+
---
+++ WordGuessApp/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+
+namespace WordGuessApp
+{
+    public partial class Form1 : Form
+    {
+        //Массив загаданных слов
+        static readonly string[] words = new string[] { "кошка", "огонь", "стул", "велосипед", "глагол", "солнце", "гора", "волна", "вода", "ветер", "воздух", "музыка", "еда" };
+        //Загаданное слово
+        private string setWord;
+        //Счётчик попыток
+        private int tryCounter = 0;
+        //Счётчик подсказываемых букв
+        private int charCounter = 0;
+        //Рандом для выбора случайной подсказки
+        Random randomHintNumber = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void buttonSendAnswer_Click(object sender, EventArgs e)
+        {
+            //Обновляем цвет в случае победы
+            richTextBoxHistory.BackColor = Color.White;
+
+            //Читаем введённое слово
+            string wordCompare = textBoxGuessWord.Text;
+            
+            //Проверяем наличие загаданного слова, если нет, загадываем автоматически
+            if (setWord == null)
+            {
+                buttonSetPuzzleWord.PerformClick();
+            }
+
+            //Приводим к общему виду для сравнения
+            wordCompare = wordCompare.ToLower();
+            wordCompare = wordCompare.Replace(" ", "");
+
+            //Сравниваем слова
+            bool result = CheckWord(wordCompare, setWord);
+
+            //Выводим вариант игрока
+            richTextBoxHistory.Text += $"Вы: {textBoxGuessWord.Text}\n";
+
+            //Если игрок угадал
+            if (result)
+            {
+                //Задаём зелёный фон
+                richTextBoxHistory.BackColor = Color.LightGreen;
+                richTextBoxHistory.Text += "Верно! Поиграем ещё? Я загадал новое слово!\n";
+                setWord = SetPuzzleWord();
+            }
+            //Если игрок не угадал
+            else
+            {
+                tryCounter++;
+                richTextBoxHistory.Text += "Нет, не то слово\n";
+                if (tryCounter > 3)
+                {
+                    //Задаём номер подсказки
+                    int hintNumber = randomHintNumber.Next(1, 3);
+
+                    switch (hintNumber)
+                    {
+                        case 1:
+                            //Сравниваем слова по длинне
+                            if(wordCompare.Length > setWord.Length)
+                            {
+
+                                richTextBoxHistory.Text += "Окей, загаданное слово короче...\n";
+                            }
+                            else if (wordCompare.Length == setWord.Length)
+                            {
+                                richTextBoxHistory.Text += "Окей, загаданное слово такой же длинны...\n";
+                            }
+                            else
+                            {
+                                richTextBoxHistory.Text += "Окей, загаданное слово длиннее...\n";
+                            }
+                            break;
+                        case 2:
+                            for (int i = charCounter; i < setWord.Length; )
+                            {
+                                //Подсказываем первую букву
+                                if (i == 0)
+                                {
+                                    richTextBoxHistory.Text += $"Подсказка: первая буква - {setWord.Substring(i, 1)}\n";
+                                    charCounter++;
+                                    break;
+                                }
+                                //Подсказываем слудующие буквы
+                                else
+                                {
+                                    richTextBoxHistory.Text += $"Лааадно, добавляем буквы... - {setWord.Substring(0, i+1)}\n";
+                                    charCounter++;
+                                    break;
+                                }
+                            }
+                            break;
+                    }
+                }
+            }
+            //Очищаем поле ввода
+            textBoxGuessWord.Clear();
+        }
+
+        private void textBoxGuessWord_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+ 
+            //Метод для сравнения строк
+            public static bool CheckWord(string wordAnswer, string wordPuzzle)
+            {
+                if (wordAnswer == wordPuzzle)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+ 
+        //Загадывание случайного слова из массива
+        private static string SetPuzzleWord() {
+            Random randomNumber = new Random();
+            int wordIndex = randomNumber.Next(0, words.Length);
+            return words[wordIndex];
+        }
+
+        private void buttonSetPuzzleWord_Click(object sender, EventArgs e)
+        {
+            setWord = SetPuzzleWord();
+        }
+        /// <summary>
+        /// По нажатию на Enter нажимаем на кнопку "Отправить"
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void textBoxGuessWord_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                buttonSendAnswer.PerformClick();
+            }
+        }
+    }
+
+
+}
